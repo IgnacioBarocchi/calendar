54baba012fe40f6999a56e28f4e91d90fe2a738f
Ignacio Barocchi <ignaciob@wix.com>
2023-09-12 15:12:36 +0300

    add folder structure

--- vanillajs/scripts/store/index.js
+++ vanillajs/scripts/store/index.js
@@ -23,3 +23,19 @@ const localStorageService = () => {
 
   return { getEvents, setEvent };
 };
+
+window.addEventListener("beforeunload", () => {
+  sessionStorage.clear();
+});
+
+const sessionStorageService = () => {
+  getWeekIndex = () => {
+    return Number(sessionStorage.getItem("weekIndex"));
+  };
+
+  setWeekIndex = (weekIndex) => {
+    sessionStorage.setItem("weekIndex", weekIndex + "");
+  };
+
+  return { getWeekIndex, setWeekIndex };
+};
