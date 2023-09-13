8c567e4c2eb1bc3d3852187f51d4bbb9bea8e61f
Ignacio Barocchi <ignaciob@wix.com>
2023-09-13 13:36:07 +0300

    update event creation logic

---
+++ vanillajs/scripts/components/TimeSlotEvent/index.js
@@ -0,0 +1,30 @@
+// !rename to startDateTime
+const TimeSlotEvent = (calendarEvent, timeSlotElement) => {
+  const { stage, title } = calendarEvent || {};
+  const classNameByEventStage = {
+    upcoming: "time-slot-event-upcoming",
+    ongoing: "time-slot-event-ongoing",
+    past: "time-slot-event-past",
+    draft: "time-slot-event-draft",
+  }[stage];
+
+  appendElements(
+    [
+      createElement2(
+        `
+        <div class="${classNameByEventStage}">
+            <span>${title}</span>
+        </div>
+    `
+      ),
+    ],
+    timeSlotElement
+  );
+
+  // const eventElement = document.createElement("div");
+  // eventElement.classList.add("time-slot-event");
+  // eventElement.appendChild(document.createTextNode(eventThumbnailPlaceHolder));
+
+  // timeSlotElement.appendChild(eventElement);
+  // return eventElement;
+};
--- vanillajs/scripts/handlers/timeSlotHandler.js
+++ vanillajs/scripts/handlers/timeSlotHandler.js
@@ -13,50 +13,36 @@ const styleModal = (clientY, clientX) => {
   modal.style.cssText = modalCss;
 };
 
-const populateEventRecordOnSlotClick = () => {
-  // !clear first
-  const startDate = document.querySelector("#start-datetime");
-  const today = new Date();
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, "0");
-  const day = String(today.getDate()).padStart(2, "0");
-  const hours = String(today.getHours()).padStart(2, "0");
-  const minutes = String(today.getMinutes()).padStart(2, "0");
-  const formattedDateTime = `${year}-${month}-${day}T${hours}:${minutes}`;
-  startDate.value = formattedDateTime;
+// !rename to startDateTime
+const populateEventRecordOnSlotClick = (startTime, endTime) => {
+  const startDateTimeField = document.querySelector("#start-datetime");
+  const endDateTimeField = document.querySelector("#end-datetime");
+
+  startDateTimeField.value = formatDateToDateInputValue(startTime);
+  endDateTimeField.value = formatDateToDateInputValue(endTime);
 };
 
-const getDraftEvent = (timeSlotElement) => {
-  const { time } = JSON.parse(timeSlotElement.dataset.dayTime);
-  const eventThumbnailPlaceHolder = `(no title), ${time}`;
+const getDraftEventFrom = (timeSlotElement) => {
+  const slotDateTime = new Date(timeSlotElement.dataset.dayTime);
+  const startTime = new Date(timeSlotElement.dataset.dayTime);
+  const endTime = new Date(slotDateTime.setMinutes(30));
 
-  const eventElement = document.createElement("div");
-  eventElement.classList.add("time-slot-event");
-  eventElement.appendChild(document.createTextNode(eventThumbnailPlaceHolder));
-
-  timeSlotElement.appendChild(eventElement);
-  return eventElement;
-};
-
-// ! add event commitment logic.
-// ? draft > pending > approved
-const createEvent = (timeSlotElement) => {
-  // const modal = document.querySelector("#event-modal");
-  // // if (modal.open) return;
-  // const eventElement = document.createElement("div");
-  // eventElement.classList.add("time-slot-event");
-  // eventElement.appendChild(
-  //   document.createTextNode(timeSlotElement.dataset.dayTime)
-  // );
-  // timeSlotElement.appendChild(eventElement);
+  return {
+    stage: "draft",
+    title: `(no title), ${startTime.getHours()}`,
+    startTime,
+    endTime,
+  };
 };
 
 const deleteEvent = () => {};
+
 document.querySelectorAll("[data-day-time]").forEach((element) => {
   element.addEventListener("click", function (event) {
     const { clientY, clientX } = event;
     styleModal(clientY, clientX);
-    const draftEvent = getDraftEvent(this);
-    populateEventRecordOnSlotClick();
+    const draftEvent = getDraftEventFrom(this);
+    TimeSlotEvent(draftEvent, this);
+    populateEventRecordOnSlotClick(draftEvent.startTime, draftEvent.endTime);
   });
 });
