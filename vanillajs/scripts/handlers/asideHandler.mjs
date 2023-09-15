a83e6b39ca907ea241cebf7c7a7bd11bf703ce0d
Ignacio Barocchi <ignaciob@wix.com>
2023-09-15 10:05:44 +0300

    add site grid layout

--- vanillajs/scripts/handlers/asideHandler.mjs
+++ vanillajs/scripts/handlers/asideHandler.mjs
@@ -4,16 +4,15 @@ import TimeSlotEvent from "../components/TimeSlotEvent/index.js";
 document
   .querySelector("#aside-create-event-button")
   .addEventListener("click", () => {
-    // !horrible
-    TimeSlotEvent(
-      {
-        stage: "draft",
-        title: `(no title), ${new Date().getHours()}`,
-        startTime: new Date(),
-        endTime: new Date(new Date().setMinutes(30)),
-      },
-      this
-    );
+    alert("a");
+    const draftEvent = {
+      stage: "draft",
+      title: `(no title), ${new Date().getHours()}`,
+      startDateTime: new Date(),
+      endDateTime: new Date(new Date().setMinutes(30)),
+    };
+
+    TimeSlotEvent(draftEvent, this);
 
     CreateEventModal(draftEvent, [clientEvent.clientY, clientEvent.clientX]);
   });
