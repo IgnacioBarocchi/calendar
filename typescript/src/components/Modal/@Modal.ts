a54301070a912020f403659a02d531c37238e00d
Ignacio Barocchi <ignaciob@wix.com>
2023-09-20 14:56:51 +0300

    style modal

--- typescript/src/components/Modal/@Modal.ts
+++ typescript/src/components/Modal/@Modal.ts
@@ -3,10 +3,12 @@ export type Position = Readonly<[number, number]>;
 export default abstract class Modal {
   modalElement: HTMLElement;
   closeButtonElement: HTMLElement;
+  draggableElement: HTMLElement;
 
   constructor(modalElement: HTMLElement, closeButtonElement: HTMLElement) {
     this.modalElement = modalElement;
     this.closeButtonElement = closeButtonElement;
+    this.draggableElement = modalElement.querySelector('.draggable-header')!;
 
     this.closeButtonElement.addEventListener(
       'click',
@@ -18,6 +20,46 @@ export default abstract class Modal {
         });
         this.hide();
       }.bind(this),
+    );
+
+    this.enableDraggin();
+  }
+
+  private enableDraggin() {
+    let initX = 0,
+      initY = 0,
+      firstX = 0,
+      firstY = 0;
+
+    function dragIt(this: HTMLElement, e: MouseEvent) {
+      if (!this.parentElement) return;
+      this.parentElement.style.left = initX + e.pageX - firstX + 'px';
+      this.parentElement.style.top = initY + e.pageY - firstY + 'px';
+    }
+
+    this.draggableElement.addEventListener(
+      'mousedown',
+      function (this: HTMLElement, e: MouseEvent) {
+        e.preventDefault();
+        const element = this;
+        if (!element.parentElement) return;
+
+        initX = element.parentElement.offsetLeft;
+        initY = element.parentElement.offsetTop;
+        firstX = e.pageX;
+        firstY = e.pageY;
+
+        element.addEventListener('mousemove', dragIt, false);
+
+        window.addEventListener(
+          'mouseup',
+          function () {
+            element.removeEventListener('mousemove', dragIt, false);
+          },
+          false,
+        );
+      },
+      false,
     );
   }
 
