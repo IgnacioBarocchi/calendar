8af413da15afac2730a2859eb85b9b545bf1b7be
Ignacio Barocchi <ignaciob@wix.com>
2023-09-21 13:37:59 +0300

    add get events json server method

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -3,6 +3,12 @@ import CalendarHeaderRow from './components/CalendarHeaderRow/CalendarHeaderRow'
 import CalendarMonth from './components/CalendarMonth/CalendarMonth';
 import ClockHand from './components/ClockHand/ClockHand';
 import NavigationControls from './controls/NavigationControls';
+import getEvents from './lib/getEvents';
+
+(async function () {
+  const data = await getEvents();
+  console.log(data);
+})();
 
 const today = new Date();
 const calendaeHeaderRowComponent = CalendarHeaderRow.getInstance();
