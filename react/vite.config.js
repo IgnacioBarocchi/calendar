d42d84530b64ba2e44861b2bb017a2935aadd1d1
Ignacio Barocchi <ignaciob@wix.com>
2023-10-09 17:53:29 +0300

    add vite config

--- react/vite.config.js
+++ react/vite.config.js
@@ -6,4 +6,8 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   // plugins: [react(), million.vite()],
   plugins: [react()],
+  // base: './',
+  server: {
+    cors: false,
+  },
 });
